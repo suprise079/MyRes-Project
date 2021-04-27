b71d6b3e195d6ec83f1aee6c45bbfb2f1a8d7e64
Suprise079 <ns.ngoveni079@gmail.com>
2021-04-27 14:24:21 +0200

    updated file system

---
+++ JavaScript_files/respage.js
@@ -0,0 +1,24 @@
+
+
+//Get write review button
+var reviewBtn = document.getElementById('writeReview');
+
+//Get form div
+var form = document.getElementById('form');
+
+//Get send button
+var send = document.getElementById('send');
+
+//Send event listener
+function change(){
+    console.log(form);
+    form.className = "show";
+    console.log("showing")
+}
+
+
+
+
+
+    
+
