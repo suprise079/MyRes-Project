ad22d4dff85b8e34062611ce7be6f4c850e1c726
Malebo Moleleki <75163080+malebo19@users.noreply.github.com>
2021-04-30 13:58:31 +0200

    updated changes

--- JavaScript_files/respage.js
+++ JavaScript_files/respage.js
@@ -16,8 +16,29 @@ function change(){
 }
 
 function enlargeMap(){
+    map.className = "enlarge";
 
 }
+
+window.onload = function() { // add window.onload here and set it euqal to a function
+    // Get the modal
+    var enlarge = document.getElementById('mapEnlarge');
+  
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var map = document.getElementById('mapContainer');
+    map.onclick = function() {
+      enlarge.style.display = "block";
+    }
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsId("close")[0];
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  } // close the function
+  
 
 
 
