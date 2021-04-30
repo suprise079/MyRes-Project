7a65a66623b3a6ce5dee4e8a327b2329e39cb746
LETHABO MAKOPO <79568509+Lethabo787@users.noreply.github.com>
2021-04-30 14:29:00 +0200

    Add files via upload

---
+++ html_files/respage.js
@@ -0,0 +1,40 @@
+
+
+//Get write review button
+var reviewBtn = document.getElementById('writeReview');
+
+//Get form div
+var form = document.getElementById('form');
+
+//Get send button
+var send = document.getElementById('send');
+
+//Send event listener
+function change(){
+    console.log(form);
+    form.className = "show";
+    console.log("showing")
+}
+
+//onclick enlarge images
+//sections, pictures, soon as the pictures are to be inserted
+function change1(){
+    document.getElementById('streetview');
+}
+function change2(){
+    document.getElementById('img1');
+}
+function change3(){
+    document.getElementById('img2');
+}
+function change4(){
+    document.getElementById('img3');
+}
+function change5(){
+    document.getElementById('img4'); 
+}
+function change5(){
+    document.getElementById('img5'); 
+}
+    
+
