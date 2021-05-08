e0a2dcede34fe7e536ff152c9c7140f32cb70ea7
Suprise079 <ns.ngoveni079@gmail.com>
2021-05-08 02:50:58 +0200

    updated homepage

--- database/reses_information.php
+++ database/reses_information.php
@@ -2,10 +2,11 @@
 
 
 /*Res information collected from the database*/
-$photos = array('pictures\login_image.jpg');
+$photos = array('pictures\RichmondChill.jpeg');
 $res_name = array('RichMond corner');
 $sharing = array('2-4 rooms');
 $location = array("apk");
+$prices = array("NSFAS Accredited");
 
 
  ?>
