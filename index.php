320c4ba4aa9dadd727d00926cf39628b299b0326
Suprise079 <ns.ngoveni079@gmail.com>
2021-05-22 18:02:06 +0200

    updated

--- index.php
+++ index.php
@@ -32,14 +32,8 @@
        <div class="row col-lg-6">
            <a href=#contacts class="header_links col-lg-3">Contacts</a>
            <a href=#about_us class="header_links col-lg-3">About us</a>
-<<<<<<< HEAD
            <a href="" id="Apply" class="col-lg-3">Apply</a>
            <button id="login" class="col-lg-3">Login</button>
-
-=======
-           <button id="Apply" class="col-lg-3" >Apply</button>
-           <button id="login" class="col-lg-3">Login</button>
->>>>>>> 0772c8dffb516a43f71ce0f3c2ce7dbc623656b2
        </div>
     </header>
     <?php require 'php_files/login.php'; ?>
